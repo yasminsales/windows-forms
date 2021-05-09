9491fc81e0a6f70fae340b5add2a4fa12054605f
Yasmin Sales <teacheryasminsales@gmail.com>
2021-05-09 18:09:22 -0300

    Forms no Visual Studio com CRUD

---
+++ aula_27_04_21/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace aula_27_04_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_cnpj = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_nome = new System.Windows.Forms.TextBox();
+            this.text_rua = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_salvar = new System.Windows.Forms.Button();
+            this.text_codigo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_cep = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_atualizar = new System.Windows.Forms.Button();
+            this.button_deletar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CNPJ";
+            // 
+            // text_cnpj
+            // 
+            this.text_cnpj.Location = new System.Drawing.Point(126, 168);
+            this.text_cnpj.Name = "text_cnpj";
+            this.text_cnpj.Size = new System.Drawing.Size(150, 31);
+            this.text_cnpj.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // text_nome
+            // 
+            this.text_nome.Location = new System.Drawing.Point(127, 217);
+            this.text_nome.Name = "text_nome";
+            this.text_nome.Size = new System.Drawing.Size(150, 31);
+            this.text_nome.TabIndex = 3;
+            // 
+            // text_rua
+            // 
+            this.text_rua.Location = new System.Drawing.Point(126, 265);
+            this.text_rua.Name = "text_rua";
+            this.text_rua.Size = new System.Drawing.Size(150, 31);
+            this.text_rua.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rua";
+            // 
+            // button_salvar
+            // 
+            this.button_salvar.Location = new System.Drawing.Point(354, 110);
+            this.button_salvar.Name = "button_salvar";
+            this.button_salvar.Size = new System.Drawing.Size(112, 34);
+            this.button_salvar.TabIndex = 6;
+            this.button_salvar.Text = "Salvar";
+            this.button_salvar.UseVisualStyleBackColor = true;
+            this.button_salvar.Click += new System.EventHandler(this.Button_salvar_Click);
+            // 
+            // text_codigo
+            // 
+            this.text_codigo.Location = new System.Drawing.Point(127, 112);
+            this.text_codigo.Name = "text_codigo";
+            this.text_codigo.Size = new System.Drawing.Size(150, 31);
+            this.text_codigo.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Codigo";
+            // 
+            // text_cep
+            // 
+            this.text_cep.Location = new System.Drawing.Point(126, 319);
+            this.text_cep.Name = "text_cep";
+            this.text_cep.Size = new System.Drawing.Size(150, 31);
+            this.text_cep.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CEP";
+            // 
+            // button_atualizar
+            // 
+            this.button_atualizar.Location = new System.Drawing.Point(354, 180);
+            this.button_atualizar.Name = "button_atualizar";
+            this.button_atualizar.Size = new System.Drawing.Size(112, 34);
+            this.button_atualizar.TabIndex = 7;
+            this.button_atualizar.Text = "Atualizar";
+            this.button_atualizar.UseVisualStyleBackColor = true;
+            this.button_atualizar.Click += new System.EventHandler(this.Button_atualizar_Click);
+            // 
+            // button_deletar
+            // 
+            this.button_deletar.Location = new System.Drawing.Point(354, 244);
+            this.button_deletar.Name = "button_deletar";
+            this.button_deletar.Size = new System.Drawing.Size(112, 34);
+            this.button_deletar.TabIndex = 8;
+            this.button_deletar.Text = "Deletar";
+            this.button_deletar.UseVisualStyleBackColor = true;
+            this.button_deletar.Click += new System.EventHandler(this.Deletar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(584, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(360, 225);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_deletar);
+            this.Controls.Add(this.button_atualizar);
+            this.Controls.Add(this.text_cep);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.text_codigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_salvar);
+            this.Controls.Add(this.text_rua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.text_nome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.text_cnpj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_cnpj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_nome;
+        private System.Windows.Forms.TextBox text_rua;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_salvar;
+        private System.Windows.Forms.TextBox text_codigo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_cep;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_atualizar;
+        private System.Windows.Forms.Button button_deletar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ aula_27_04_21/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula_27_04_21
+{
+    public partial class Form1 : Form
+    {
+        // Monta o caminho ate o arquivo de banco de dados
+        readonly string caminhoBancoDados = @"C:\Users\yasmi\source\repos\aula_27_04_21\aula_27_04_21\database\database.mdf";
+
+        // Monta a string de conexao com o banco de dados 
+        //private string 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // void = sem retorno, nao vai retornar nada para quem esta chamando o metodo 
+
+        private int ExecutarSql(string sql, List<(string name, object value)> parametros)
+        {
+            var connString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                              @$"AttachDbFilename={caminhoBancoDados};" +
+                                              @"Integrated Security=True;Connect Timeout=30";
+
+            // Executa a conexao com o Banco de Dados
+            var connection = new System.Data.SqlClient.SqlConnection
+            {
+                ConnectionString = connString
+            };
+            // Abre a conexao
+            connection.Open();
+
+            try
+            {
+                var command = connection.CreateCommand();
+
+                // Monta o string SQL para a tabela clientes
+                command.CommandText = sql;
+
+                foreach (var (name, value) in parametros)
+                {
+                    command.Parameters.AddWithValue(name, value);
+
+                }
+
+                int rowAffected = command.ExecuteNonQuery();
+
+                return rowAffected;
+            }
+            finally
+            {
+                // Fecha a conexao com o banco de dados
+                connection.Close();
+            }
+
+        }
+
+        private void LimparDados()
+        {
+            text_codigo.Clear();
+            text_cnpj.Clear();
+            text_nome.Clear();
+            text_rua.Clear();
+            text_cep.Clear();
+        }
+
+        private void Button_salvar_Click(object sender, EventArgs e)
+        {
+            var codigo = text_codigo.Text;
+            var cnpj = text_cnpj.Text;
+            var nome = text_nome.Text;
+            var rua = text_rua.Text;
+            var cep = text_cep.Text;
+
+            try
+            {
+                var sql = "INSERT INTO Clientes  " +
+                    " (Codigo, CNPJ, Razao_Social,Rua, Cep)" +
+                    "VALUES " +
+                    " (@Codigo, @Cnpj, @Razao_Social, @Rua, @Cep);";
+                // Cria o comando do SQL na conexao aberta
+
+                var parametros = new List<(string name, object value)>
+                {
+                    ("Codigo", codigo),
+                    ("Cnpj", cnpj),
+                    ("Razao_Social", nome),
+                    ("Rua", rua),
+                    ("Cep", cep)
+                };
+
+                // Metodo para executar o comando SQL que nao retorna dados
+                int rowAffected = ExecutarSql(sql, parametros);
+
+                MessageBox.Show("Dados inseridos com sucesso");
+
+                // Limpa os dados em tela 
+
+                LimparDados();
+
+                PopularDataGrid();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ocorreu um erro: " + exception.Message);
+
+            }
+        }
+
+        private void Button_atualizar_Click(object sender, EventArgs e)
+        {
+            var codigo = text_codigo.Text;
+            var cnpj = text_cnpj.Text;
+            var nome = text_nome.Text;
+            var rua = text_rua.Text;
+            var cep = text_cep.Text;
+
+            var connString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                           @$"AttachDbFilename={caminhoBancoDados};" +
+                                           @"Integrated Security=True;Connect Timeout=30";
+
+            // Executa a conexao com o Banco de Dados
+            var connection = new System.Data.SqlClient.SqlConnection
+            {
+                ConnectionString = connString
+            };
+            // Abre a conexao
+            connection.Open();
+            try
+            {
+                // Cria o comando do SQL na conexao aberta
+                var command = connection.CreateCommand();
+
+                // Monta o string SQL para a tabela clientes
+                command.CommandText = "UPDATE Clientes SET   " +
+                    " Codigo = @Codigo, Razao_Social = @Razao_Social," +
+                    " Rua = @Rua, Cep = @Cep" +
+                    " WHERE CNPJ = @Cnpj;";
+
+                command.Parameters.AddWithValue("Codigo", codigo);
+                command.Parameters.AddWithValue("Cnpj", cnpj);
+                command.Parameters.AddWithValue("Razao_Social", nome);
+                command.Parameters.AddWithValue("Rua", rua);
+                command.Parameters.AddWithValue("Cep", cep);
+
+                // Metodo para executar o comando SQL que nao retorna dados
+                int rowAffected = command.ExecuteNonQuery();
+                if (rowAffected == 0)
+                {
+                    MessageBox.Show("Nenhuma linha encontrada.");
+                }
+                else
+                {
+                    MessageBox.Show("Dados alterados com sucesso");
+                }
+
+                connection.Close();
+
+                // Limpa os dados em tela 
+                LimparDados();
+
+                PopularDataGrid();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ocorreu um erro: " + exception.Message);
+
+                // Fecha a conexao com o banco de dados
+                connection.Close();
+            }
+        }
+
+        private void Deletar_Click(object sender, EventArgs e)
+        {
+            var cnpj = text_cnpj.Text;
+
+            var connString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                           @$"AttachDbFilename={caminhoBancoDados};" +
+                                           @"Integrated Security=True;Connect Timeout=30";
+
+            // Executa a conexao com o Banco de Dados
+            var connection = new System.Data.SqlClient.SqlConnection
+            {
+                ConnectionString = connString
+            };
+            // Abre a conexao
+            connection.Open();
+            try
+            {
+                // Cria o comando do SQL na conexao aberta
+                var command = connection.CreateCommand();
+
+                command.CommandText = "DELETE FROM Clientes WHERE cnpj = @Cnpj;";
+
+                command.Parameters.AddWithValue("Cnpj", cnpj);
+
+                // Metodo para executar o comando SQL que nao retorna dados
+                int rowAffected = command.ExecuteNonQuery();
+                if (rowAffected == 0)
+                {
+                    MessageBox.Show("Nenhuma linha encontrada.");
+                }
+                else
+                {
+                    MessageBox.Show("Dados excluidos com sucesso");
+                }
+
+                connection.Close();
+
+                // Limpa os dados em tela 
+                LimparDados();
+
+                PopularDataGrid();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Erro ao deletar: " + exception.Message);
+                connection.Close();
+            }
+        }
+
+        private void PopularDataGrid()
+        {
+            var connString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                           @$"AttachDbFilename={caminhoBancoDados};" +
+                                           @"Integrated Security=True;Connect Timeout=30";
+
+            var connection = new System.Data.SqlClient.SqlConnection
+            {
+                ConnectionString = connString
+            };
+            // Abre a conexao
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT Codigo, CNPJ, " +
+                                         "Razao_Social, Rua, Cep" +
+                                  " FROM Clientes;";
+
+            var reader = command.ExecuteReader();
+            var dataTable = new DataTable();
+            dataTable.Load(reader);
+
+            this.dataGridView1.DataSource = dataTable;
+
+            connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopularDataGrid();
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var dataTable = (DataTable)this.dataGridView1.DataSource;
+
+            var row = dataTable.Rows[e.RowIndex];
+
+            text_codigo.Text = (string)row[0];
+            text_cnpj.Text = (string)row[1];
+            text_nome.Text = (string)row[2];
+            text_rua.Text = (string)row[3];
+            text_cep.Text = (string)row[4];
+        }
+    }
+
+}
+
